9de86336b73fadfe2ade22b3c4cb01de71f3e29b
Del Bosque Julian <julian.delbosque@yahoo.fr>
2020-10-14 11:30:07 +0200

    modif

--- controllers/product.js
+++ controllers/product.js
@@ -1,68 +1,72 @@
 const Product = require("../models/Product");
 
-const product ={
+const product = {
+  content: (req, res) => {
+    Product.find({}, "title imagexs price", (err, products) => {
+      if (err) {
+        res.status(500).json({
+          message: "not good",
+        });
+      } else {
+        res.json(products);
+      }
+    });
+  },
 
-    content: (req, res) => {
-        
-        Product.find({}, "title imagexs price", (err, products) => {
-            if (err){ 
-                res.status(500).json({
-                    message: "not good"
-                })
-                }else{
-                    res.json(products)
-                }
-        })
-    },
+  detail: (req, res) => {
+    Product.findOne({ _id: req.query.id }, (err, product) => {
+      if (err) {
+        res.status(500).json({
+          message: "not good",
+        });
+      } else {
+        res.json(product);
+      }
+    });
+  },
 
-    detail: (req, res) => {
-        Product.findOne({_id: req.query.id}, (err, product) => {
-            if (err){ 
-                res.status(500).json({
-                    message: "not good"
-                })
-                }else{
-                    res.json(product)
-                }
-        })
-        
-    },
+  sidebar: (req, res) => {
+    Product.find({}, "title imagexs", (err, products) => {
+      if (err) {
+        res.status(500).json({
+          message: "not good",
+        });
+      } else {
+        res.json(products);
+      }
+    });
+  },
+  search: (req, res) => {
+    const searchterms = req.query.search.split(" ");
+    const filter = {
+      $or: [
+        {
+          title: {
+            $in: searchterms.map(
+              (element) => new RegExp("." + element + ".", "i")
+            ),
+          },
+        },
+        {
+          category: {
+            $in: searchterms.map(
+              (element) => new RegExp("." + element + ".", "i")
+            ),
+          },
+        },
+      ],
+    };
 
-    sidebar: (req, res) => {
-        Product.find({},"title imagexs", (err, products) => {
-            if (err) {
-                res.status(500).json({
-                    message: "not good"
-                })
-            }else{
-                res.json(products)
-            }
-        } )
-        
-    },
-    search: (req, res) => {
-        const searchterms = req.query.search.split(" ");
-        const filter = {
-            $or: [
-                {
-                    title: {$in: searchterms.map(element => new RegExp("." + element + ".", "i"))}
-                },
-                {
-                    category: {$in: searchterms.map(element => new RegExp("." + element + ".", "i"))}
-                }
-            ]
-        } 
-
-        Product.find(filter, (err, product) => {
-            if (err){ 
-                res.status(500).json({
-                    message: "not good"
-                })
-                }else{
-                    res.json(product)
-                }
-        })
-    }
-}
+    Product.find(filter, (err, product) => {
+      if (err) {
+        res.status(500).json({
+          message: "not good",
+        });
+      } else {
+        res.json(product);
+      }
+    });
+  },
+};
 
 module.exports = product;
