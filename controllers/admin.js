a6cf22bb5b73c789bbce0a20bf519cc648638a60
TheRott06 <69381950+TheRott06@users.noreply.github.com>
2020-10-15 13:31:25 +0200

    route setup

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,26 +13,41 @@ const admin = {
     });
   },
   addProduct: (req, res) => {
-    Admin.find({}, "title imagexs price category", (err, products) => {
-      if (err) {
-        res.status(500).json({
-          message: "not good",
-        });
-      } else {
-        res.json(products);
+    Admin.insertMany(
+      {
+        category: req.body.category,
+        title: req.body.title,
+        image: req.body.image,
+        imagexs: req.body.imagexs,
+        price: req.body.price,
+      },
+
+      (err, products) => {
+        if (err) {
+          res.status(500).json({
+            message: "not good",
+          });
+        } else {
+          res.json(products);
+        }
       }
-    });
+    );
   },
+
   deleteProduct: (req, res) => {
-    Admin.find({}, "title imagexs price category", (err, products) => {
-      if (err) {
-        res.status(500).json({
-          message: "not good",
-        });
-      } else {
-        res.json(products);
+    Admin.deleteOne(
+      { _id: req.body._id },
+
+      (err, products) => {
+        if (err) {
+          res.status(500).json({
+            message: "not good",
+          });
+        } else {
+          res.json("products deleted");
+        }
       }
-    });
+    );
   },
 };
 
